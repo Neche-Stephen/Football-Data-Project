27a894e4a559876444860d2be1f43e73df2413f1
chinecherem <chinecheremubawike@gmail.com>
2022-09-16 16:35:18 -0500

    Modified app to use React CreateRef, in place of query selectors

--- src/Football.jsx
+++ src/Football.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Football.css'
 
-class Football extends React.Component  {
+class Football extends Component{
+    constructor() {
+        super();
+        this.inputRef = React.createRef();
+      }
 
-    clickMe = (x) => {
-        this.props.clickMe(x)
+
+      getUsers =  async (year) => {
+        let url = `https://jsonmock.hackerrank.com/api/football_competitions?year=${year}`;
+        try {
+            let res = await fetch(url);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }    
+      }
+
+    myFunction  = async (year)=> {
+      console.log(year)
+      let users = await this.getUsers(year);
+      if (users.total === 0){
+          let html = ''
+          html = `<div data-test-id="no-result">No Matches Found</div>`
+        //   let container = document.querySelector('#myMain');
+        //   container.innerHTML = html;
+        this.inputRef.current.innerHTML = html
+          return
+      }
+      
+      let html = `<div data-test-id="total-matches class = "total_match">${'Total Matches: ' + users.total}</div>`
+      users.data.forEach((user)=> {
+          let htmlSegment = `<div className="user">
+                                  <p className = "match">${"Match " + user.name + ' won by ' + user.winner }</p>
+                          </div>`;
+
+          html += htmlSegment;
+      });
+      // let container = document.querySelector('#myMain');
+      // container.innerHTML = html;
+      this.inputRef.current.innerHTML = html
     }
+
+
     render(){
 
         return (
@@ -17,31 +55,31 @@ class Football extends React.Component  {
                                     <div>
                                         <p >Select Year</p>
                                     </div>
-                                    <button className="list-div" onClick={() => { this.clickMe(2011)}}>
+                                    <button className="list-div" onClick={() => { this.myFunction(2011)}}>
                                         <p className="list-border " >2011</p>
                                     </button>
-                                    <button className="list-div" onClick={this.clickMe.bind(this, 2012)}>
+                                    <button className="list-div" onClick={this.myFunction.bind(this, 2012)}>
                                         <p className="list-border">2012</p>
                                     </button>
-                                    <button className="list-div" onClick={() => { this.clickMe(2013)}}>
+                                    <button className="list-div" onClick={() => { this.myFunction(2013)}}>
                                         <p className="list-border">2013</p>
                                     </button>
-                                    <button className="list-div" onClick={() => { this.clickMe(2014)}}>
+                                    <button className="list-div" onClick={() => { this.myFunction(2014)}}>
                                         <p className="list-border">2014</p>
                                     </button>
-                                    <button className="list-div" onClick={() => { this.clickMe(2015)}}>
+                                    <button className="list-div" onClick={() => { this.myFunction(2015)}}>
                                         <p className="list-border">2015</p>
                                     </button>
-                                    <button className="list-div" onClick={() => { this.clickMe(2016)}}>
+                                    <button className="list-div" onClick={() => { this.myFunction(2016)}}>
                                         <p className="list-border">2016</p>
                                     </button>
-                                    <button className="list-div" onClick={() => { this.clickMe(2017)}}>
+                                    <button className="list-div" onClick={() => { this.myFunction(2017)}}>
                                         <p className="list-border">2017</p>
                                     </button>
                     
                                 </div>
                             </aside>
-                        <main className="col-9" id="myMain">
+                        <main className="col-9" id="myMain" ref={this.inputRef }>
                             
                         </main>
             
@@ -52,7 +90,8 @@ class Football extends React.Component  {
               )
 
     }
+
+    }
  
-}
 
 export default Football
